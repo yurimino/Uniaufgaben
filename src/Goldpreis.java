e24aa5d699caabe7c0ef78f6b327407c8431e1ec
yurimino <yuri@lennartz.org>
2023-10-16 22:04:21 +0200

    feat: docstring comments added

--- src/Goldpreis.java
+++ src/Goldpreis.java
@@ -6,11 +6,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+
+/**
+ * represents a collection of gold price values over time
+ */
 public class Goldpreis {
 
-
+    /**
+     * ArrayList containing Goldtagespreis objects
+     */
     private final ArrayList<Goldtagespreis> list;
 
+    /**
+     * constructor which reads date-price pairs from a file at a specified path, makes Goldtagespreis objects out of them and adds those objects to the list attribute
+     * @param dateiname String value of the relative path of the file
+     * @throws IOException from Files.readAllLines if path name is invalid
+     */
     public Goldpreis (String dateiname) throws IOException {
         this.list = new ArrayList<>();
         String[] splitLine;
@@ -36,6 +47,11 @@ public class Goldpreis {
 
     }
 
+    /**
+     * get the price of gold on the specified date
+     * @param datum date of the requested gold value
+     * @return value of gold as a double value on the given date
+     */
     public double getPreis(String datum) {
         int index = -1;
         try {
@@ -48,6 +64,12 @@ public class Goldpreis {
 
     }
 
+    /**
+     * support method for getPreis() which returns the first index at which a given date can be found
+     * @param datum String value containing the date which is being searched for
+     * @return int value of the index of the requested date
+     * @throws NoSuchElementException if the requested date does not exist in the list
+     */
     private int findIndexOfDate(String datum) {
         for (int i = 0; i < this.list.size(); i++) {
             if(this.list.get(i).datum.equals(datum)) {
@@ -57,6 +79,9 @@ public class Goldpreis {
         throw new NoSuchElementException(); //in case date doesn't exist
     }
 
+    /**
+     * prints the minimum and maximum gold values and every date on which they occurred
+     */
     public void printMinMax() {
         double[] minMax = getMinMax();
         System.out.printf("Den niedrigsten Goldpreis von %2f gab es an folgenden Tagen:\n", minMax[0]);
@@ -66,6 +91,11 @@ public class Goldpreis {
 
     }
 
+    /**
+     * support method for printMinMax() to get a list of all dates on which gold had a specific price
+     * @param price the price being filtered by as a double value
+     * @return ArrayList of Strings containing the dates on which gold had the price in question
+     */
     private ArrayList<String> getDatesByPrice(double price) {
         ArrayList<String> results = new ArrayList<>();
         for (Goldtagespreis element : list) {
@@ -76,6 +106,10 @@ public class Goldpreis {
         return results;
     }
 
+    /**
+     * support method for printMinMax() which determines the minimum and maximum price of gold in the list attribute
+     * @return array of two double values representing the minimum and maximum in the form [min, max]
+     */
     private double[] getMinMax() {
         double[] minMax = {this.list.get(0).preis,this.list.get(0).preis}; //minMax[0] is min, minMax[1] is max
         for(Goldtagespreis element : list) {
--- src/Goldtagespreis.java
+++ src/Goldtagespreis.java
@@ -1,14 +1,30 @@
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
+
+/**
+ * represents the price of gold on a specific date
+ */
 public class Goldtagespreis {
 
     //may be public
+
+    /**
+     * String value of the date in the format "yyyy-MM-dd"
+     */
     String datum;
+    /**
+     * price as a positive double value S
+     */
     double preis;
 
+    /**
+     * constructor that takes the date and price and checks them for validity before assigning them to the attributes
+     * @param datum String value containing the date in the format "yyyy-MM-dd"
+     * @param preis price as a positive double value
+     * @throws IllegalArgumentException if an invalid date has been found or the price is invalid
+     */
     public Goldtagespreis(String datum, double preis) {
 
         if(!datumIsValid(datum) || (preis < 0 && preis != -1)) { //-1 must be allowed to indicate that there was no price data for the given date
@@ -19,16 +35,26 @@ public class Goldtagespreis {
 
     }
 
+    /**
+     * override of the toString() method
+     * @return String of the format "Datum: [datum]; Preis: [preis]"
+     */
     @Override
     public String toString() {
-        return "Datum: " + this.datum + " ; Preis: " + preis;
+        return "Datum: " + this.datum + "; Preis: " + preis;
     }
 
+    /**
+     * checks if a given date is of the valid format (yyyy-MM-dd) and consists of numbers that are valid for the given date format
+     * @param datum String value containing the date to be checked
+     * @return true if the date is valid, else false
+     *
+     */
     private boolean datumIsValid(String datum){
         try {
-            SimpleDateFormat format = new SimpleDateFormat("yyy-MM-dd"); //declares the format that is being used
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd"); //declares the format that is being used
             format.setLenient(false); //ensures that only dates that really exist return true, even if the non-existent date has the correct format
-            Date parsedDate = format.parse(datum);
+            format.parse(datum); //doesn't need to be saved anywhere because it is just supposed to throw an exception if parsing isn't possible
             return true; //never reached if parsing throws exception
         } catch (ParseException e){
             System.out.println(datum + " is not valid. It's either in the wrong format or the date does not exist");
